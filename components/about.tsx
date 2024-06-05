4b864ebd5d79a12a2ee5157569c1f0706a83c180
Arnaldo Benavides <arnaldobenavides@uninorte.edu.co>
2024-06-05 11:11:19 -0500

    All done

---
+++ components/about.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3 text-lg">
+        After graduating as a <span className="font-medium">systems engineer</span>, 
+        I decided to expand my passion for programming by working as a freelancer while learning various
+        frameworks and technologies. I enrolled in various programming courses
+        at Udemy and the Colombian ICT Ministry, where I learned complete{" "}
+        <span className="font-medium">web/mobile development</span>. One of my greatest achievements was working for
+        the University of the North (Barranquilla) on an R&D project about Covid
+        19 in older adults, there I was able to develop a toolkit to support
+        health professionals to determine if a patient had chances of
+        contracting this condition. 
+        </p>
+
+        <p className="mb-3 text-lg">
+        My favorite part of programming is the
+        problem solving aspect. I <span className="underline">love</span> the feeling of finally finding a solution
+        to a problem. My main stack is <span className="font-medium">React, Next.js, Laravel and Flutter</span>. I'm
+        also familiar with TypeScript and Microsoft Azure. I am always looking
+        to learn new technologies.
+      </p>
+
+      <p className="text-lg">
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games 🎮, watching movies 🎬, and playing with my dog 🐶. I also enjoy learning
+        new recipes 🍝🍽️.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,118 @@
+import React from "react";
+import { FaLaravel, FaPhp, FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import AppBarber from "@/public/AppBarber.png";
+import NavesEspaciales from "@/public/NavesEspaciales.png";
+import ML4Covid from "@/public/ML4Covid.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Systems Engineering Degree",
+    location: "Barranquilla",
+    description:
+      "I graduated after 5 years of study. I immediately found a job as a Junior FullStack developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022",
+  },
+  {
+    title: "Junior FullStack Developer",
+    location: "Barranquilla",
+    description:
+      "I worked as a junior FullStack developer for 3 months in a hospital. My role was to remodel part of the company's internal system. During this time I improved my PHP skills.",
+    icon: React.createElement(FaPhp),
+    date: "2023",
+  },
+  {
+    title: "FullStack Developer",
+    location: "Barranquilla",
+    description:
+      "I worked as an independent provider in an R&D project at the Universidad del Norte for 6 months, there I improved my skills in Laravel and web hosting.",
+    icon: React.createElement(FaLaravel),
+    date: "2023",
+  },
+  {
+    title: "React Full-Stack Developer",
+    location: "México",
+    description:
+      "I worked as a freelance Fullstack developer for Neoseg (6 months), during that time I improved my skills in React - Electron.js - Firebase",
+    icon: React.createElement(FaReact),
+    date: "2024",
+  },
+  {
+    title: "FrontEnd Developer",
+    location: "Barranquilla",
+    description:
+      "I am now a full-time FrontEnd developer working for Infinity Tech. My stack includes React, Next.js, TypeScript, Tailwind, and more.",
+    icon: React.createElement(FaReact),
+    date: "2024 - Present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "App Barber",
+    description:
+      "Personal project that allows you to schedule and manage services in a barbershop.",
+    tags: ["PHP", "MySQL", "SCSS", "Gulp.js", "JS"],
+    imageUrl: AppBarber,
+  },
+  {
+    title: "Space Invaders",
+    description:
+      "2D arcade game made with vanilla JavaScript.",
+    tags: ["HTML", "JavaScript"],
+    imageUrl: NavesEspaciales,
+  },
+  {
+    title: "ML project for covid cases in Colombia",
+    description:
+      "Machine Learning model using real data from the first cases of Covid-19 in Colombia.",
+    tags: ["ML", "Python", "Jupyter NoteBook"],
+    imageUrl: ML4Covid,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Bootstrap",
+  "MySQL",
+  "Redux",
+  "Express",
+  "Python",
+  "PHP",
+  "Laravel",
+  "Firebase",
+  "Azure",
+  "Framer Motion",
+] as const;
